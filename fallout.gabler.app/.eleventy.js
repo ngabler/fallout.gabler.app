b6358d70471da92397c5c6a346b482be3213b7b7
ngabler <nick@gabler.app>
2022-08-22 22:03:54 -0700

    testing d3 v4

--- fallout.gabler.app/.eleventy.js
+++ fallout.gabler.app/.eleventy.js
@@ -3,4 +3,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("_headers");
   eleventyConfig.addPassthroughCopy("d3.v3.min.js");
+  eleventyConfig.addPassthroughCopy("d3.v4.min.js");
 };
