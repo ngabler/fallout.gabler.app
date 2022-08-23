3c4896691b8dc284f4d65f91ad31b93fb4025314
ngabler <nick@gabler.app>
2022-08-22 17:29:12 -0700

    adding d3 to eleventy passthrough

--- fallout.gabler.app/.eleventy.js
+++ fallout.gabler.app/.eleventy.js
@@ -2,4 +2,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("_headers");
+  eleventyConfig.addPassthroughCopy("d3.v3.min.js");
 };
